64f57fb18fa3269305edccc2e83ce43c4f7dc14a
TheHankmachine <weyehen@gmail.com>
2026-09-05 14:23:03 +0200

    *renamed kill command to shutdown

---
+++ src/commands/admin/shutdown.ts
@@ -0,0 +1,44 @@
+import { registerSlashCommand, shutdown } from '../../bot.js';
+import { MessageFlags, SlashCommandBuilder } from 'discord.js';
+import * as Constants from '../../constants.js';
+
+
+registerSlashCommand(
+    new SlashCommandBuilder().setName("shutdown")
+        .setDescription("shuts down the bot")
+        .addStringOption(option => option.setName('action')
+            .setDescription('action')
+            .setRequired(true)
+            .addChoices(
+                { name: 'restart', value: 'restart' },
+                { name: 'terminate', value: 'terminate' },
+            )
+        ),
+    async (interaction) => {
+        if (interaction.user.id != process.env.ADMIN_USER_ID) {
+            await interaction.reply({
+                content: "this is an admin only command",
+                flags: MessageFlags.Ephemeral
+            });
+            return;
+        }
+
+        const action = interaction.options.getString('action');
+        if (action == "restart") {
+            await interaction.reply("restarting...");
+            await shutdown();
+            process.exit(0);
+        }
+        
+        if (action == "terminate") {
+            let content = "terminating...";
+            if (Math.random() < 1) {
+                content = "https://klipy.com/gifs/spy-tf2-30"
+            }
+            await interaction.reply(content);
+            await shutdown();
+            process.exit(Constants.HALT_CODE);
+        }
+        
+    }
+);
